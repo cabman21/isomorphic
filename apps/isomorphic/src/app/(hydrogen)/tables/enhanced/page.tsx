b681660b825efbfbe0fd2eb010afa0dadbd6d24f
김진해 <pj.cabman21@kalmate.net>
2024-08-27 14:22:04 +0900

    update https://nestjs-mongodb-tau.vercel.app/invoices

--- apps/isomorphic/src/app/(hydrogen)/tables/enhanced/page.tsx
+++ apps/isomorphic/src/app/(hydrogen)/tables/enhanced/page.tsx
@@ -1,5 +1,5 @@
 import { routes } from '@/config/routes';
-import { invoiceData } from '@/data/invoice-data';
+// import { invoiceData } from '@/data/invoice-data';
 import InvoiceTable from '@/app/shared/invoice/invoice-list/table';
 import TableLayout from '@/app/(hydrogen)/tables/table-layout';
 import { metaObject } from '@/config/site.config';
@@ -24,7 +24,17 @@ const pageHeader = {
   ],
 };
 
-export default function EnhancedTablePage() {
+const URL = "https://nestjs-mongodb-tau.vercel.app/invoices";
+
+const getInvoices = async () => {
+  return await fetch(URL, { cache: 'no-store' })
+    .then((res) => res.json())
+    .catch((err) => "error");
+};
+
+export default async function EnhancedTablePage() {
+  const invoiceData = await getInvoices();
+  console.log("invoiceData:", invoiceData);
   return (
     <TableLayout
       title={pageHeader.title}
